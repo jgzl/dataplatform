fa0112e2ff7a64301ca08355ae8d50cb7ee4d30d
整洁架构 <li7hai26@outlook.com>
2022-10-21 22:48:35 +0800

    数据中台-增加naive前端

---
+++ admin-ui-naive/src/api/http.ts
@@ -0,0 +1,94 @@
+import {AxiosResponse} from 'axios'
+import {App} from '@vue/runtime-core'
+import request from './axios.config'
+
+export interface HttpOption {
+  url: string
+  data?: any
+  method?: string
+  headers?: any
+  beforeRequest?: () => void
+  afterRequest?: () => void
+}
+
+export interface Response<T = any> {
+  totalSize: number | 0
+  code: number
+  msg: string
+  data: T
+}
+
+function http<T = any>({ url, data, method, headers, beforeRequest, afterRequest }: HttpOption) {
+  const successHandler = (res: AxiosResponse<Response<T>>) => {
+    if (res.data.code === 200) {
+      return res.data
+    }
+    throw new Error(res.data.msg || '请求失败，未知异常')
+  }
+  const failHandler = (error: Response<Error>) => {
+    afterRequest && afterRequest()
+    throw new Error(error.msg || '请求失败，未知异常')
+  }
+  beforeRequest && beforeRequest()
+  method = method || 'GET'
+  const params = Object.assign(typeof data === 'function' ? data() : data || {}, {})
+  return method === 'GET'
+    ? request.get(url, { params }).then(successHandler, failHandler)
+    : request.post(url, params, { headers: headers }).then(successHandler, failHandler)
+}
+
+export function get<T = any>({
+  url,
+  data,
+  method = 'GET',
+  beforeRequest,
+  afterRequest,
+}: HttpOption): Promise<Response<T>> {
+  return http<T>({
+    url,
+    method,
+    data,
+    beforeRequest,
+    afterRequest,
+  })
+}
+
+export function post<T = any>({
+  url,
+  data,
+  method = 'POST',
+  headers,
+  beforeRequest,
+  afterRequest,
+}: HttpOption): Promise<Response<T>> {
+  return http<T>({
+    url,
+    method,
+    data,
+    headers,
+    beforeRequest,
+    afterRequest,
+  })
+}
+
+function install(app: App): void {
+  app.config.globalProperties.$http = http
+
+  app.config.globalProperties.$get = get
+
+  app.config.globalProperties.$post = post
+}
+
+export default {
+  install,
+  get,
+  post,
+}
+
+declare module '@vue/runtime-core' {
+  // 为 `this.$` 提供类型声明
+  interface ComponentCustomProperties {
+    $get: <T>(options: HttpOption) => Promise<Response<T>>
+    $post: <T>(options: HttpOption) => Promise<Response<T>>
+  }
+}
