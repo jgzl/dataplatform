00aa31abae4d7ee3b495f87d2b9fb03482439d13
整洁架构 <li7hai26@outlook.com>
2022-12-02 21:55:15 +0800

    数据中台-替换代码

--- common/common-model/src/main/java/cn/cleanarch/dp/system/feign/RemoteClientDetailsService.java
+++ common/common-model/src/main/java/cn/cleanarch/dp/system/feign/RemoteClientDetailsService.java
@@ -1,22 +1,3 @@
-/*
- *
- *      Copyright (c) 2018-2025, li7hai26@outlook.com All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- *  this list of conditions and the following disclaimer.
- *  Redistributions in binary form must reproduce the above copyright
- *  notice, this list of conditions and the following disclaimer in the
- *  documentation and/or other materials provided with the distribution.
- *  Neither the name of the pig4cloud.com developer nor the names of its
- *  contributors may be used to endorse or promote products derived from
- *  this software without specific prior written permission.
- *  Author: li7hai26@outlook.com (wangiegie@gmail.com)
- *
- */
-
 package cn.cleanarch.dp.system.feign;
 
 import cn.cleanarch.dp.common.core.constant.SecurityConstants;
